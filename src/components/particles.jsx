213f6d60f9fed04204ed340234be1e3f535754df
SAMIRAN_47 <98637819+hackerX-Sam@users.noreply.github.com>
2025-09-28 00:43:49 +0530

    Fix tsparticles version compatibility

--- src/components/particles.jsx
+++ src/components/particles.jsx
@@ -1,5 +1,5 @@
 import { useCallback } from "react";
-import Particles from "react-tsparticles";
+import Particles from "@tsparticles/react";
 import { loadSlim } from "@tsparticles/slim";
 
 const ParticleBackground = () => {
